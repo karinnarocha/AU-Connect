11e99976b6a95e90f5c941ee6183a0ee9b8b218e
Karinna Rocha - NurVIX <75399228+karinnarocha@users.noreply.github.com>
2023-05-04 13:31:19 -0400

    Update config.py

--- aep/config.py
+++ aep/config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'Alliance@Spring2023'
+    SECRET_KEY = '**********'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir, 'aep.db')
     
