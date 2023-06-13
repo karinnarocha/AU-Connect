0e11344c9eae42e21e8740a55e483660dac69ae0
Karinna Rocha - NurVIX <75399228+karinnarocha@users.noreply.github.com>
2023-06-13 14:26:26 -0400

    Update config.py

--- aep/config.py
+++ aep/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '**********'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir, 'aep.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir, '******')
     
